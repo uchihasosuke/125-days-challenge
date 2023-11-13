e8e9cc3ac4fe10f8046b50b6af4acd85f740ae30
muzaffar-7869 <105149031+muzaffar-7869@users.noreply.github.com>
2023-11-13 12:37:21 +0530

    Update and rename 12 days to 125 days/day1/prime_num.py

---
+++ 125 days/day1/prime_num.py
@@ -0,0 +1,15 @@
+
+#write a program to check if a number is prime or not
+def prime(num):
+    if(num<=1):
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if(num%i==0):
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
