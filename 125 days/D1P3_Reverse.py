9ba2b552757b3466b834c258921b64de04fe6b07
muzaffar-7869 <105149031+muzaffar-7869@users.noreply.github.com>
2023-11-13 12:42:17 +0530

    Create D1P3_Reverse.py

---
+++ 125 days/D1P3_Reverse.py
@@ -0,0 +1,8 @@
+#create a python function to reverse a given string
+def reverse(string):
+    return string[::-1]
+
+string="Siuuuuuuu"
+reverse=reverse(string)
+print(f"String before Reversing: {string}")
+print(f"After Reversing: {reverse}")
