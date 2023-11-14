7b0f7dbb7b3c63fa2d73d7755356dd6dd7cd447d
MadihaMulla <146825777+MadihaMulla@users.noreply.github.com>
2023-11-14 13:00:43 +0530

    Create Program_4.py

---
+++ 125 days/Day 9/Program_4.py
@@ -0,0 +1,22 @@
+#Develop a Python program that demonstrates the concept of inheritance with multiple classes.
+
+class Name: name="
+
+def myfunc1 (self): 
+  print(self.name)
+
+class Surname: surname=""
+
+def myfunc2(self): 
+  print(self.surname)
+
+class Student(Name,Surname):
+
+def mainfunc(self): 
+  print("Name::",self.name) 
+  print("Surname::",self.surname)
+
+stud-Student() 
+stud.name="Madiha" 
+stud.surname="Mulla" 
+stud.mainfunc()
