753c895dec0dd5de4f00f9ebdc3e8427fa4fabf7
priyankaudanur <149400665+priyankaudanur@users.noreply.github.com>
2023-11-14 15:21:36 +0530

    Create Program 1.py

---
+++ 125 days/Day 12/Program 1.py
@@ -0,0 +1,22 @@
+#Implement a Python class representing a basic stack ADT with push, pop, and peek methods.
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack is empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Stack is empty")
